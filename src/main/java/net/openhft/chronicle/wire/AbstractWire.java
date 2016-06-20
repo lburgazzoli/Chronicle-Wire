a3e55bf5ca8237547658b4f8f2c2232585abbffc
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-06-20 18:05:55 +0100

    reverted my last change to "abstract client" as this broke a couple of the tests, requires further investigation before I re-apply the change.

--- src/main/java/net/openhft/chronicle/wire/AbstractWire.java
+++ src/main/java/net/openhft/chronicle/wire/AbstractWire.java
@@ -220,12 +220,12 @@ public abstract class AbstractWire implements Wire {
             return pos;
         }
 
-        if (lastPosition == null)
+     //   if (lastPosition == null)
             return writeHeader0(length, timeout, timeUnit);
 
-        headerNumber = Long.MIN_VALUE;
-        bytes.writePosition(lastPosition.getValue());
-        return writeHeader0(length, timeout, timeUnit);
+   //     headerNumber = Long.MIN_VALUE;
+    //    bytes.writePosition(lastPosition.getValue());
+     //   return writeHeader0(length, timeout, timeUnit);
     }
 
     private long writeHeader0(int length, long timeout, TimeUnit timeUnit) throws TimeoutException, EOFException {
